81797f4df85fca733a15d137dcbb3e1a25525798
760946378@qq.com <yxz0921>
2019-05-10 18:13:06 +0800

    初次提交

--- gen/src/main/java/com/hc/gen/utils/FileUtils.java
+++ gen/src/main/java/com/hc/gen/utils/FileUtils.java
@@ -13,8 +13,8 @@ import java.util.List;
 /**
  * 文件操作工具类
  * 实现文件的创建、删除、复制、压缩、解压以及目录的创建、删除、复制、压缩解压等功能
- * @author ThinkGem
- * @version 2015-3-16
+ * @author xzyuan
+ * @version 2019-3-16
  */
 public class FileUtils extends org.apache.commons.io.FileUtils {
 	
