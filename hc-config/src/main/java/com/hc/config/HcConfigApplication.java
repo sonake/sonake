cc18e12b250634356433fbac6e0f101f83b6b7b2
yuanxingzhi <yxz0921>
2019-06-27 20:53:30 +0800

    调整目录结构

--- hc-config/src/main/java/com/hc/config/HcConfigApplication.java
+++ hc-config/src/main/java/com/hc/config/HcConfigApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class HcConfigApplication {
 
     public static void main(String[] args) {
