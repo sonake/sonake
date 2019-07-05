dc7817346fde0192e7518643f2bc3649c27d8d3d
yuanxingzhi <yxz0921>
2019-07-05 16:45:26 +0800

    gateway

---
+++ config/hc-gateway/src/main/java/com/hc/gateway/HcGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcGatewayApplication.class, args);
+    }
+
+}
