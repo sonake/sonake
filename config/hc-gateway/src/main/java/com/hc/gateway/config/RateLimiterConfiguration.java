c6adc3e59b4e875f68e0bd3873f13e67a9125d26
wzh9644 <15136129644@139.com>
2019-07-16 17:11:56 +0800

    网关服务初始化

---
+++ config/hc-gateway/src/main/java/com/hc/gateway/config/RateLimiterConfiguration.java
@@ -0,0 +1,19 @@
+package com.hc.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author lengleng
+ * @date 2019/2/1
+ * 路由限流配置
+ */
+@Configuration
+public class RateLimiterConfiguration {
+	@Bean(value = "remoteAddrKeyResolver")
+	public KeyResolver remoteAddrKeyResolver() {
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+	}
+}
---
+++ config/hc-gateway/src/main/java/com/hc/gateway/config/RouterFunctionConfiguration.java
@@ -0,0 +1,37 @@
+
+package com.hc.gateway.config;
+
+import com.hc.gateway.handler.HystrixFallbackHandler;
+import com.hc.gateway.handler.ImageCodeHandler;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+
+/**
+ * @author lengleng
+ * @date 2019/2/1
+ * 路由配置信息
+ */
+@Slf4j
+@Configuration
+@AllArgsConstructor
+public class RouterFunctionConfiguration {
+	private final HystrixFallbackHandler hystrixFallbackHandler;
+	private final ImageCodeHandler imageCodeHandler;
+
+	@Bean
+	public RouterFunction routerFunction() {
+		return RouterFunctions.route(
+			RequestPredicates.path("/fallback")
+				.and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), hystrixFallbackHandler)
+			.andRoute(RequestPredicates.GET("/code")
+				.and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), imageCodeHandler);
+
+	}
+
+}
