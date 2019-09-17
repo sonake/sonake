a8bf528af1e054b3fd808fe00a3d61001e3a6735
yuanxingzhi <760946378@qq.com>
2019-09-17 17:52:08 +0800

    模板更新

---
+++ hc-server-management/hc-config/src/main/java/com/hc/hcconfig/HcConfigApplication.java
@@ -0,0 +1,13 @@
+package com.hc.hcconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcConfigApplication.class, args);
+    }
+
+}
---
+++ hc-server-management/hc-register/src/main/java/com/hc/register/config/FebsRegisterWebSecurityConfigure.java
@@ -0,0 +1,14 @@
+package com.hc.register.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class FebsRegisterWebSecurityConfigure extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
