fa1d8620dccd54d951659dfa6418730b1ea7b3c1
yuanxingzhi <760946378@qq.com>
2019-09-30 14:58:21 +0800

    架构更新

---
+++ hc-register/src/main/java/com/hc/register/HcRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.hc.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcRegisterApplication.class, args);
+    }
+
+}
