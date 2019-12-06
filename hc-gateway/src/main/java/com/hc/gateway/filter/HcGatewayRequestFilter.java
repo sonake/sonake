b2f79d4bc241d5b6efaee13fc9e5d4861b1c49e2
yuanxingzhi <760946378@qq.com>
2019-12-06 11:31:33 +0800

    dev-2.0 init

--- hc-gateway/src/main/java/com/hc/gateway/filter/HcGatewayRequestFilter.java
+++ hc-gateway/src/main/java/com/hc/gateway/filter/HcGatewayRequestFilter.java
@@ -1,15 +1,25 @@
 package com.hc.gateway.filter;
 
+import com.hc.common.result.Rets;
+import com.hc.common.utils.ToolUtil;
+import com.hc.gateway.properties.HcGatewayProperties;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import com.hc.common.constant.HcConstant;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.Base64Utils;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author ：xzyuan
@@ -20,6 +30,11 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @Component
 public class HcGatewayRequestFilter extends ZuulFilter {
+
+    @Autowired
+    private HcGatewayProperties properties;
+
+    private AntPathMatcher pathMatcher = new AntPathMatcher();
 
     /**
      * 对应Zuul生命周期的四个阶段：pre、post、route和error，
@@ -65,6 +80,33 @@ public class HcGatewayRequestFilter extends ZuulFilter {
         String method = request.getMethod();
         String uri = request.getRequestURI();
         log.info("请求URI：{}，HTTP Method：{}，请求IP：{}，ServerId：{}", uri, method, host, serviceId);
+
+        // 禁止外部访问资源实现
+        boolean shouldForward = true;
+        String forbidRequestUri = properties.getForbidRequestUri();
+        String[] forbidRequestUris = StringUtils.splitByWholeSeparatorPreserveAllTokens(forbidRequestUri, ",");
+        if (forbidRequestUris != null && ArrayUtils.isNotEmpty(forbidRequestUris)) {
+            for (String u : forbidRequestUris) {
+                if (pathMatcher.match(u, uri)) {
+                    shouldForward = false;
+                }
+            }
+        }
+        if (!shouldForward) {
+            HttpServletResponse response = requestContext.getResponse();
+            try {
+
+                ToolUtil.makeResponse(
+                        response, MediaType.APPLICATION_JSON_UTF8_VALUE,
+                        HttpServletResponse.SC_FORBIDDEN, Rets.failure("该URI不允许外部访问")
+                );
+                requestContext.setSendZuulResponse(false);
+                requestContext.setResponse(response);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 
         byte[] token = Base64Utils.encode((HcConstant.ZUUL_TOKEN_VALUE).getBytes());
         requestContext.addZuulRequestHeader(HcConstant.ZUUL_TOKEN_HEADER, new String(token));
---
+++ hc-gateway/src/main/java/com/hc/gateway/properties/HcGatewayProperties.java
@@ -0,0 +1,23 @@
+package com.hc.gateway.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ：xzyuan
+ * @date ：Created in 2019/12/5 11:20
+ * @description：禁止外部访问的url
+ * @version: 1.0
+ */
+@RefreshScope
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "hc.gateway")
+public class HcGatewayProperties {
+    /**
+     * 禁止外部访问的 URI，多个值的话以逗号分隔
+     */
+    private String forbidRequestUri;
+}
